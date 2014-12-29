b067b765da505635dc849eb5f0d3247a863a2cb9
Tawanda-m <tminya@gmail.com>
2014-12-29 13:07:20 +0200

    changed database settings

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -9,14 +9,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Or use an alternate database backend.
-        'NAME': 'tmp.db',                       # Path to sqlite3 database file.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rest',
+        'USER': 'test1',
+        'PASSWORD': 'test1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
